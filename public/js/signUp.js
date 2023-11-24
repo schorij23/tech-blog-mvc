b29f5ace1ed33bd5f0c6ab85e331f151e669f9b6
schorij23 <schorij23@gmail.com>
2023-11-24 12:40:56 -0500

    undu mistake

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,30 +1,30 @@
 const signUpFormHandler = async (event) =>{
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      try { 
-        const response = await fetch('/api/users/', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      document.location.replace('/dashboard');
-  
-      // if (response.ok) {
-      //   document.location.replace('/dashboard');
-      // } else {
-      //   alert('Failed to sign up');
-      // }
-        } catch (error) {
-          console.log(error);
-          alert('Failed to sign up')
-         
-  };
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && password) {
+    try { 
+      const response = await fetch('/api/users/', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    document.location.replace('/dashboard');
+
+    // if (response.ok) {
+    //   document.location.replace('/dashboard');
+    // } else {
+    //   alert('Failed to sign up');
+    // }
+      } catch (error) {
+        console.log(error);
+        alert('Failed to sign up')
+       
+};
 }
 }
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signUpFormHandler);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signUpFormHandler);
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,30 +1,21 @@
 const sequelize = require('../config/connection');
 const { User, Comments, Blog } = require('../models');
 
-const blogData = require('./blogData.json');
-const commentData = require('./commentData.json');
 const userData = require('./userData.json');
-// Function to seed the database with data
+// const commentsData = require('./Comments.json');
+// const blogData = require('./blogData');
+
+// ? Still missing comments and blog Data??
 const seedDatabase = async () => {
-    // Synchronize the Sequelize models with the database and force it to recreate tables
     await sequelize.sync({ force: true });
-    // Bulk create users with individual hooks for hashing passwords
-    const users = await User.bulkCreate(userData, {
-      individualHooks: true,
-      returning: true,
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
     });
-    // Loop through blogData and create blogs with a random user_id
-    for (const blog of blogData) {
-      await Blog.create({
-        ...blog,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
-      });
-    }
-    // Bulk create comments
-    const comments = await Comments.bulkCreate(commentData, {
-      returning: true,
-    });
-  
+
+ 
+
     process.exit(0);
 };
 
