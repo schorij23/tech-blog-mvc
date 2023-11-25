e4d341222e257a540bc8d85abe4cd8985db50fed
schorij23 <schorij23@gmail.com>
2023-11-25 12:06:08 -0500

    css blog typos

--- models/index.js
+++ models/index.js
@@ -1,22 +1,26 @@
 const User = require('./User');
-const Comment = require('./Comment');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
+
 
 
 User.hasMany(Blog, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
 Blog.hasMany(Comment, {
     foreignKey: 'blog_id'
 });
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+;
 
 module.exports = { User, Blog, Comment };
