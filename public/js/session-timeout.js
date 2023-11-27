9071aa92726beaf1046d25d4371043ee830fe560
schorij23 <schorij23@gmail.com>
2023-11-27 16:45:46 -0500

    button

--- public/js/session-timeout.js
+++ public/js/session-timeout.js
@@ -1,37 +1,39 @@
-// session-timeout.js
-
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded.'); // Check if the script is loaded
+    // Check if the script is loaded
+    console.log('Script loaded.');
 
     let idleTime = 0;
-    const idleInterval = 6000; // Set the idle time in milliseconds (e.g., 1 minute)
-    let promptShown = false; // Variable to track if the prompt has been shown
+     // Set the idle time in milliseconds (60000 = 1 minute)
+    const idleInterval = 6000;
+     // Variable to track if the prompt has been shown
+    let promptShown = false;
 
-    // Increment the idle time every minute
+    // Increment and check the idle time 
     setInterval(function () {
         timerIncrement();
-        console.log('Idle time:', idleTime); // Check the idle time in the console
+        console.log('Idle time:', idleTime);
     }, idleInterval);
 
-    // Reset the idle time on any user activity
+    // Reset the idle time check for mouse movement
     document.addEventListener('mousemove', function () {
         resetIdleTime();
-        console.log('Mouse moved.'); // Check if mouse movement is detected
+        console.log('Mouse moved.');
     });
-
+    // Reset the idle time check for key press
     document.addEventListener('keydown', function () {
         resetIdleTime();
-        console.log('Key pressed.'); // Check if a key is pressed
+        console.log('Key pressed.');
     });
-
+    // Set idel time ahnd check if prompt is not shown
     function timerIncrement() {
         idleTime += 1;
         if (idleTime >= 1 && !promptShown) {
-            // Set the idle threshold (e.g., 5 minutes) and check if the prompt has not been shown
             // Show a modal prompt on the page
             showLoginModal();
-            console.log('Prompt shown.'); // Check if the prompt is shown
-            promptShown = true; // Set the variable to true after showing the prompt
+            // Check if the prompt is shown
+            console.log('Prompt shown.');
+            // Set the variable to true after showing the prompt
+            promptShown = true;
         }
     }
 
@@ -48,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="loginModalLabel">Session Timeout</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="resetPrompt()"></button>
                         </div>
                         <div class="modal-body">
                             Please log to add, update, or delete posts
